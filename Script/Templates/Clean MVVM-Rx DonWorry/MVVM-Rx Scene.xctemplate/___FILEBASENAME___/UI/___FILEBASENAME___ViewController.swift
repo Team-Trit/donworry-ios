98b12501791f6c797181147752b97d93f7b3f54f
wody-d <maxwodyd98@gmail.com>
2022-08-04 16:42:56 +0900

    ✨템플릿 스크립트 추가

---
+++ Script/Templates/Clean MVVM-Rx DonWorry/MVVM-Rx Scene.xctemplate/___FILEBASENAME___/UI/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import UIKit
+import RxCocoa
+import RxSwift
+
+final class ___VARIABLE_sceneName___ViewController: BaseViewController {
+
+    #warning("ReactorKit으로 변환 필요 + RxFlow를 통해 주입하기")
+    let viewModel = ___VARIABLE_sceneName___ViewModel()
+    private var disposeBag: DisposeBag = DisposeBag()
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+
+        attributes()
+        layout()
+    }
+
+
+}
+
+extension ___VARIABLE_sceneName___ViewController {
+    private func attributes() {
+
+    }
+
+    private func layout() {
+
+    }
+}
---
+++ Script/Templates/Clean MVVM-Rx DonWorry/MVVM-Rx Scene.xctemplate/___FILEBASENAME___/UI/___FILEBASENAME___ViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+final class ___VARIABLE_sceneName___ViewModel: BaseViewModel {
+
+    var disposeBag: DisposeBag = DisposeBag()
+    
+    init() {}
+
+}
