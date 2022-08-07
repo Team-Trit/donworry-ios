9a70357a667b876f701f49807431ca4169c6a51c
wody-d <maxwodyd98@gmail.com>
2022-08-07 19:09:15 +0900

    ✨ 디자인시스템 Color 추가

---
+++ Projects/DesignSystem/Sources/Color/UIColor+.swift
@@ -0,0 +1,17 @@
+//
+//  UIColor+.swift
+//  DesignSystem
+//
+//  Created by Woody on 2022/08/07.
+//  Copyright © 2022 Tr-iT. All rights reserved.
+//
+
+import UIKit
+import DonWorryExtensions
+
+public extension UIColor {
+    class func donworry(_ color: Pallete) -> UIColor? {
+        guard let bundle = Bundle(identifier: "com.Tr-iT.DesignSystem") else { return nil}
+        return UIColor(named: color.rawValue, in: bundle, compatibleWith: nil)
+    }
+}
