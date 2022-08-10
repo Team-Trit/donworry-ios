771ac6deee812fbebb8edfdff749da692dcb2ba5
Asher3576 <dud3576@naver.com>
2022-08-10 21:35:57 +0900

    feat: CreateRoom Ui작업

--- Projects/DonWorry/Sources/EnterRoom/UI/EnterRoomViewController.swift
+++ Projects/DonWorry/Sources/EnterRoom/UI/EnterRoomViewController.swift
@@ -14,18 +14,59 @@ import RxSwift
 
 
 final class EnterRoomViewController: BaseViewController {
-
-    #warning("ReactorKit으로 변환 필요 + RxFlow를 통해 주입하기")
+    
     let viewModel = EnterRoomViewModel()
 
+    var hasSetPointOrigin = false
+    var pointOrigin: CGPoint?
+    
+    @IBOutlet weak var slideIdicator: UIView!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var subscribeButton: UIView!
+    
+    
     public override func viewDidLoad() {
         super.viewDidLoad()
 
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(panGestureRecognizerAction))
+        view.addGestureRecognizer(panGesture)
+        
+        slideIdicator.roundCorners(.allCorners, radius: 10)
+        subscribeButton.roundCorners(.allCorners, radius: 10)
+        
         attributes()
         layout()
     }
-
-
+    
+    override func viewDidLayoutSubviews() {
+        if !hasSetPointOrigin {
+            hasSetPointOrigin = true
+            pointOrigin = self.view.frame.origin
+        }
+    }
+    
+    @objc func panGestureRecognizerAction(sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: view)
+        
+        // Not allowing the user to drag the view upward
+        guard translation.y >= 0 else { return }
+        
+        // setting x as 0 because we don't want users to move the frame side ways!! Only want straight up or down
+        view.frame.origin = CGPoint(x: 0, y: self.pointOrigin!.y + translation.y)
+        
+        if sender.state == .ended {
+            let dragVelocity = sender.velocity(in: view)
+            if dragVelocity.y >= 1300 {
+                self.dismiss(animated: true, completion: nil)
+            } else {
+                // Set back to original position of the view controller
+                UIView.animate(withDuration: 0.3) {
+                    self.view.frame.origin = self.pointOrigin ?? CGPoint(x: 0, y: 400)
+                }
+            }
+        }
+    }
+    
 }
 
 extension EnterRoomViewController {
