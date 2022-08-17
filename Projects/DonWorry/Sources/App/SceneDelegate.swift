415d39e0408bc2656051ed74fa31207b941ea940
Chanhee Jeong <chan420420@gmail.com>
2022-08-18 03:40:47 +0900

    🩹 SecneDelegate 홈으로 원복

--- Projects/DonWorry/Sources/App/SceneDelegate.swift
+++ Projects/DonWorry/Sources/App/SceneDelegate.swift
@@ -19,13 +19,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         UserDefaults.standard.writeCodable(User.dummyUser2, key: .user)
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        let rootViewController = PaymentCardNameEditViewController(type: .update)
-//        rootViewController.reactor = HomeViewReactor()
+        let rootViewController = HomeViewController()
+        rootViewController.reactor = HomeViewReactor()
         let navigationController = UINavigationController(rootViewController: rootViewController)
-//        navigationController.setNavigationBarHidden(true, animated: false)
-        window.rootViewController = navigationController
+        navigationController.setNavigationBarHidden(true, animated: false)
+        window.rootViewController = rootViewController
         window.makeKeyAndVisible()
         self.window = window
     }
 }
+
 
