9e5c25d86165bea782a93b692a81c92a652a98ff
hankyulee <77485339+kyustudyo@users.noreply.github.com>
2022-08-27 22:02:08 +0900

    feat: PaymentCardDetail init

---
+++ Projects/DonWorry/Sources/PaymentCardDetail/PaymentCardDetail/UI/PaymentCardDetailViewController.swift
@@ -0,0 +1,39 @@
+//
+//  PaymentCardDetailViewController.swift
+//  App
+//
+//  Created by Hankyu Lee on 2022/08/27.
+//  Copyright (c) 2022 Tr-iT. All rights reserved.
+//
+
+import UIKit
+import BaseArchitecture
+import ReactorKit
+import RxCocoa
+import RxSwift
+
+final class PaymentCardDetailViewController: BaseViewController, View {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //setUI()
+    }
+
+    func bind(reactor: PaymentCardDetailViewReactor) {
+        //binding here
+    }
+
+}
+
+// MARK: setUI
+
+extension PaymentCardDetailViewController {
+
+    private func attributes() {
+
+    }
+
+    private func layout() {
+
+    }
+}
