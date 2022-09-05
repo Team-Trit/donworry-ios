928ab5ed617b0262c1cc206e3171fa898763d085
wody-d <maxwodyd98@gmail.com>
2022-09-05 12:43:30 +0900

    ♻️ 메소드 나누기

--- Projects/DonWorry/Sources/UI/SpaceName/SpaceNameReactor.swift
+++ Projects/DonWorry/Sources/UI/SpaceName/SpaceNameReactor.swift
@@ -59,14 +59,7 @@ final class SpaceNameReactor: Reactor {
         case .viewDidLoad:
             return .just(.setup)
         case .didTapButton:
-            switch currentState.type {
-            case .rename(let spaceID):
-                return spaceService.editSpaceName(id: spaceID, name: currentState.spaceName)
-                    .map { _ in .routeTo(.pop) }
-            case .create:
-                return spaceService.createSpace(title: currentState.spaceName)
-                    .map { .routeTo(.cardList($0)) }
-            }
+            return request(by: currentState.type)
         case .didTapBackButton:
             return .just(.routeTo(.pop))
         case .typeTextField(let name):
@@ -76,23 +69,43 @@ final class SpaceNameReactor: Reactor {
 
     func reduce(state: State, mutation: Mutation) -> State {
         var newState = state
-         switch mutation {
-         case .setup:
-             switch currentState.type {
-             case .create:
-                 newState.title = "정산방을\n생성해볼까요?"
-                 newState.placeHolderText = "정산방 이름을 입력하세요."
-             case .rename:
-                 newState.title = "정산방\n이름을 설정해주세요"
-                 newState.placeHolderText = "ex) MC2 번개모임"
-             }
-         case .routeTo(let step):
-             newState.step = step
-         case .changeSpaceName(let name):
-             newState.spaceName = name
-         }
+        switch mutation {
+        case .setup:
+            newState.title = titleMessage(of: currentState.type)
+            newState.placeHolderText = placeHolderText(of: currentState.type)
+        case .routeTo(let step):
+            newState.step = step
+        case .changeSpaceName(let name):
+            newState.spaceName = name
+        }
         return newState
     }
 
+    private func request(by type: RoomNameEditViewType) -> Observable<Mutation> {
+        switch type {
+        case .create:
+            return spaceService.createSpace(title: currentState.spaceName).map { .routeTo(.cardList($0)) }
+        case .rename(let spaceID):
+            return spaceService.editSpaceName(id: spaceID, name: currentState.spaceName).map { _ in .routeTo(.pop) }
+        }
+    }
+
+    private func titleMessage(of type: RoomNameEditViewType) -> String {
+        switch type {
+        case .create:
+            return "정산방을\n생성해볼까요?"
+        case .rename:
+            return "정산방\n이름을 설정해주세요"
+        }
+    }
+
+    private func placeHolderText(of type: RoomNameEditViewType) -> String {
+        switch type {
+        case .create:
+            return "정산방 이름을 입력하세요."
+        case .rename:
+            return "ex) MC2 번개모임"
+        }
+    }
     private let spaceService: SpaceService
 }
