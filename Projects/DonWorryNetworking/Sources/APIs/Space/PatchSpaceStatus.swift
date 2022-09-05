d66405ae10ea5ec707fcaec4643b75e285633a82
wody-d <maxwodyd98@gmail.com>
2022-09-05 13:38:12 +0900

    ✨ 정산방 상태 변경 API 추가

---
+++ Projects/DonWorryNetworking/Sources/APIs/Space/PatchSpaceStatus.swift
@@ -0,0 +1,40 @@
+//
+//  PatchSpaceStatus.swift
+//  DonWorryNetworking
+//
+//  Created by Woody on 2022/09/05.
+//  Copyright © 2022 Tr-iT. All rights reserved.
+//
+
+import Foundation
+import Moya
+
+public struct PatchSpaceStatusAPI: ServiceAPI {
+    public typealias Response = DTO.PatchSpaceStatus
+    public var request: Request
+    public init(request: Request) {
+        self.request = request
+    }
+    public var path: String { return "/spaces/status" }
+    public var method: Method = .patch
+    public var task: Task {
+        .requestJSONEncodable(request)
+    }
+    public var headers: [String : String]? {
+        return ["Authorization" : "Bearer \(UserDefaults.standard.string(forKey: "accessToken") ?? "")"]
+    }
+}
+
+extension PatchSpaceStatusAPI {
+    public struct Request: Encodable {
+        public var id: Int
+        public var status: String
+        public init(
+            id: Int,
+            status: String
+        ) {
+            self.id = id
+            self.status = status
+        }
+    }
+}
