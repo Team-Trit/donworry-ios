ec4e41d57e127082cf6bfd4865488567a46d36a8
kimscastle <uisong26559287@gmail.com>
2022-09-09 15:02:23 +0900

    :clown_face: 목데이터 위치수정

--- Projects/DonWorry/Sources/UI/SentMoneyDetail/Views/SentMoneyTableViewCell.swift
+++ Projects/DonWorry/Sources/UI/SentMoneyDetail/Views/SentMoneyTableViewCell.swift
@@ -9,35 +9,6 @@
 import UIKit
 import DesignSystem
 
-public struct SendingMoneyInfoViewModel {
-    let name: String
-    let date: String
-    let totalAmount: Int
-    let totalUers: Int
-    let myAmount: Int
-}
-
-public struct SendingMoneyProgressInfoViewModel {
-    let name: String
-    let myAmount: Int
-    let totalAmount: Int
-    
-}
-
-let myMoneyInfo: [SendingMoneyInfoViewModel] = [
-    SendingMoneyInfoViewModel(name: "우디네당구장", date: "05/25", totalAmount: 184000, totalUers: 4, myAmount: 44000),
-    SendingMoneyInfoViewModel(name: "우디네당구장", date: "05/25", totalAmount: 152000, totalUers: 2, myAmount: 76000),
-    SendingMoneyInfoViewModel(name: "안녕하세요", date: "03/45", totalAmount: 123000, totalUers: 5, myAmount: 12000),
-    SendingMoneyInfoViewModel(name: "졸려죽겟다", date: "03/12", totalAmount: 39000, totalUers: 3, myAmount: 13000)
-]
-
-let myMoneyDetailInfo: [SendingMoneyProgressInfoViewModel] = [
-    SendingMoneyProgressInfoViewModel(name: "유쓰", myAmount: 20000, totalAmount: 120000),
-    SendingMoneyProgressInfoViewModel(name: "애셔", myAmount: 100000, totalAmount: 120000),
-    SendingMoneyProgressInfoViewModel(name: "찰리", myAmount: 1000, totalAmount: 10000),
-    SendingMoneyProgressInfoViewModel(name: "gg", myAmount: 2140, totalAmount: 2345)
-]
-
 class SentMoneyTableViewCell: UITableViewCell {
 
 
