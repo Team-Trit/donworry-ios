52e4acf5dacec25c9ebf638c38575361ba879444
hankyulee <77485339+kyustudyo@users.noreply.github.com>
2022-09-14 16:22:51 +0900

    feat: 사진 첨부 없을때 멘트 나오도록

--- Projects/DonWorry/Sources/UI/PaymentCardDetail/PaymentCardDetailViewController.swift
+++ Projects/DonWorry/Sources/UI/PaymentCardDetail/PaymentCardDetailViewController.swift
@@ -108,6 +108,14 @@ final class PaymentCardDetailViewController: BaseViewController, View {
         return view
     }()
     
+    private let noFilesLabel: UILabel = {
+        let label = UILabel()
+        label.text = "첨부 내역이 없습니다."
+        label.font = .designSystem(weight: .regular, size: ._13)
+        label.textColor = .designSystem(.gray818181)
+        return label
+    }()
+    
     private let fileCollectionViewFlowLayout: UICollectionViewFlowLayout = {
         let view = UICollectionViewFlowLayout()
         view.minimumLineSpacing = 15
@@ -214,7 +222,10 @@ final class PaymentCardDetailViewController: BaseViewController, View {
 
         reactor.state.map { $0.imgURLs }
             .observe(on: MainScheduler.instance)
-            .subscribe(onNext: { [weak self] _ in
+            .subscribe(onNext: { [weak self] in
+                if $0.count == 0 {
+                    self?.showNoFilesMent()
+                }
                 self?.fileCollectionView.reloadData()
             }).disposed(by: disposeBag)
 
@@ -358,6 +369,12 @@ extension PaymentCardDetailViewController: UICollectionViewDelegate, UICollectio
         default:
             return CGSize(width: 84, height: 84)
         }
+    }
+    
+    private func showNoFilesMent() {
+        self.filesContainerView.addSubview(noFilesLabel)
+        self.noFilesLabel.anchor(top: filesContainerView.topAnchor, left: filesContainerView.leftAnchor, bottom: filesContainerView.bottomAnchor, right: filesContainerView.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0)
+        noFilesLabel.textAlignment = .center
     }
 }
 
