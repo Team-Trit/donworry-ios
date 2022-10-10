da22daa1fbca23f17900c6e553da22cefe6fabb3
Chanhee Jeong <chan420420@gmail.com>
2022-10-10 15:15:25 +0900

    🧹 테스트용 코드 삭제

--- Projects/DonWorry/Sources/UI/PaymentCardIconEdit/PaymentCardIconEditViewReactor.swift
+++ Projects/DonWorry/Sources/UI/PaymentCardIconEdit/PaymentCardIconEditViewReactor.swift
@@ -56,8 +56,7 @@ final class PaymentCardIconEditViewReactor: Reactor {
     func mutate(action: Action) -> Observable<Mutation> {
         switch action {
         case .viewDidLoad:
-            return .just(.updateCategories(.init()))
-//            return getAllCategoriesUseCase.getAllCategories().map { .updateCategories($0) }
+            return getAllCategoriesUseCase.getAllCategories().map { .updateCategories($0) }
         case .didTapBackButton:
             return .just(.routeTo(.pop))
         case .didTapNextButton:
@@ -76,22 +75,7 @@ final class PaymentCardIconEditViewReactor: Reactor {
         var newState = state
         switch mutation {
         case .updateCategories(let categories):
-            newState.categoryCellViewModel = [
-                PaymentIconCellViewModel(id: 1, imageName: "ic_chicken", imageURL: ""),
-                PaymentIconCellViewModel(id: 2, imageName: "ic_coffee", imageURL: ""),
-                PaymentIconCellViewModel(id: 3, imageName: "ic_wine", imageURL: ""),
-                PaymentIconCellViewModel(id: 4, imageName: "ic_shoppingcart", imageURL: ""),
-                PaymentIconCellViewModel(id: 5, imageName: "ic_movie", imageURL: ""),
-                PaymentIconCellViewModel(id: 6, imageName: "ic_spoonknife", imageURL: ""),
-                PaymentIconCellViewModel(id: 7, imageName: "ic_cake", imageURL: ""),
-                PaymentIconCellViewModel(id: 8, imageName: "ic_car", imageURL: ""),
-                PaymentIconCellViewModel(id: 9, imageName: "ic_icecream", imageURL: ""),
-                PaymentIconCellViewModel(id: 10, imageName: "ic_beer", imageURL: ""),
-                PaymentIconCellViewModel(id: 11, imageName: "ic_mic", imageURL: ""),
-                PaymentIconCellViewModel(id: 12, imageName: "ic_bacon", imageURL: "")
-            ]
-            
-//            newState.categoryCellViewModel = categories.map { .init(id: $0.id, imageName: $0.name, imageURL: $0.imgURL) }
+            newState.categoryCellViewModel = categories.map { .init(id: $0.id, imageName: $0.name, imageURL: $0.imgURL) }
         case .routeTo(let step):
             newState.step = step
         case .updateCategory(let categoryID):
